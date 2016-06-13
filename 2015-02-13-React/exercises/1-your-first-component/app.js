f955dcee3dad493ced4ab64ae1a34de2227ea44e
wja123 <w.j.alag@gmail.com>
2016-06-13 18:39:35 -0400

    committing Front End Masters React Workshop

---
+++ 2015-02-13-React/exercises/1-your-first-component/app.js
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+// Excercise:
+//
+// Render `DATA` to the page
+// - put the title in an h1
+// - only render mexican food (hint: arrays have a "filter" method)
+// - sort the items in alphabetical order by name
+//   (might want to use `sort-by` https://github.com/staygrimm/sort-by#example)
+////////////////////////////////////////////////////////////////////////////////
+
+var React = require('react');
+var sortBy = require('sort-by');
+
+var DATA = {
+  title: 'Menu',
+  items: [
+    { id: 1, name: 'tacos', type: 'mexican' },
+    { id: 2, name: 'burrito', type: 'mexican' },
+    { id: 3, name: 'tostada', type: 'mexican' },
+    { id: 4, name: 'hush puppies', type: 'southern' }
+  ]
+};
+
+var Menu = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>
+        {DATA.title}
+        </h1>
+        <ul>
+          {DATA.items.filter((item) => item.type === 'mexican').sort(sortBy('name')).map((item) => <li key={item.id}>{item.name}</li>)}
+        </ul>
+      </div>
+      )
+  }
+});
+
+React.render(<Menu/>, document.body, () => {
+  require('./tests').run();
+});
+
